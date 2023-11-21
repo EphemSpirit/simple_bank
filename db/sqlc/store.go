9e15afa1bbdefae4936d1b0605cc4ae2fa758ab6
Drew Hund <languageinvestigator@gmail.com>
2023-11-21 16:47:31 -0500

    first tx and test

---
+++ db/sqlc/store.go
@@ -0,0 +1,93 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// functions to generate transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// execute function within a db tx
+func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+
+	err = fn(q)
+	if err != nil {
+		rbErr := tx.Rollback()
+		if rbErr != nil {
+			return fmt.Errorf("tx error: %q - rb err: %q", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountId int64 `json:"from_account_id"`
+	ToAccountId int64 `json:"to_account_id"`
+	Amount int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer Transfer `json:"transfer"`
+	FromAccount Account `json:"from_account"`
+	ToAccount Account `json:"to_account"`
+	FromEntry Entry `json:"from_entry"`
+	ToEntry Entry `json:"to_entry"`
+}
+
+func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := s.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountId,
+			ToAccountID: arg.ToAccountId,
+			Amount: arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountId,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountId,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: update balances
+
+		return nil
+	})
+
+	return result, err
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db, Queries: New(db)}
+}
